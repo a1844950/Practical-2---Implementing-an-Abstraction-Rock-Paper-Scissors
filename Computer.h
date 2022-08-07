f8a2849f674c7ba7ec441accce619b4478b66fd9
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-07 15:03:38 +0800

    Update Computer.h

--- Computer.h
+++ Computer.h
@@ -1,1 +1,12 @@
+#ifndef CHEGINTHIS_COMPUTER_H
+#define CHEGINTHIS_COMPUTER_H
 
+
+class Computer {
+public:
+    Computer( ); // constructor
+    char makeMove( );
+};
+
+
+#endif//CHEGINTHIS_COMPUTER_H
