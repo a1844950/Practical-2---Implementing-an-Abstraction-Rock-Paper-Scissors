704a38c3df6f56a6815168adebde3b9ec67d30ba
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-07 15:07:22 +0800

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -1,1 +1,16 @@
+#ifndef CHEGINTHIS_REFEREE_H
+#define CHEGINTHIS_REFEREE_H
 
+
+#include "Computer.h"
+#include "Human.h"
+class Referee {
+public:
+    // constructor
+    Referee();
+    // returns the outcome for player1: 'W', 'L' or 'T' (Win, Lose, Tie)
+    char refGame(Human player1, Computer player2);
+};
+
+
+#endif//CHEGINTHIS_REFEREE_H
