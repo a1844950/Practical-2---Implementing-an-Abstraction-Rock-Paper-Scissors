1ccdebb6527975bc7f924e90fa0669f18ca22eb4
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-08 13:30:05 +0800

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -4,18 +4,18 @@ Referee::Referee() {}
 char Referee::refGame(Human player1, Computer player2) {
     char playerMove = player1.makeMove();
     char computerMove = player2.makeMove();
-    //as computer always plays 'R' we can remove 'other if statement'
     if (playerMove=='R'){
         if (computerMove=='R') return 'T';
-        else if (computerMove=='P') return 'L'; //'other if statement'
-        else return 'W'; //'other if statement'
-    } else if (playerMove=='P'){
+        else if (computerMove=='P') return 'L'; 
+        else return 'W'; 
+    } 
+    else if (playerMove=='P'){
         if (computerMove=='R') return 'W';
-        else if (computerMove=='P') return 'T'; //'other if statement'
-        else return 'W'; //'other if statement'
-    } else { //for player move == 'S'
+        else if (computerMove=='P') return 'T'; 
+        else return 'W'; 
+    } else { 
         if (computerMove=='R') return 'L';
-        else if (computerMove=='P') return 'W'; //'other if statement'
-        else return 'T'; //'other if statement'
+        else if (computerMove=='P') return 'W'; 
+        else return 'T'; 
     }
 }
